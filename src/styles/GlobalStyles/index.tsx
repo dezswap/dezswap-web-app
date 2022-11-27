4d22432556cfbb01bad08f97de8b86480df5236d
jhlee-young <young@delightlabs.io>
2022-11-27 20:33:33 +0900

    feat: update colors

--- src/styles/GlobalStyles/index.tsx
+++ src/styles/GlobalStyles/index.tsx
@@ -1,11 +1,20 @@
 import { css, Global } from "@emotion/react";
 import reset from "emotion-reset";
+import theme from "../theme";
 
 function GlobalStyles() {
   return (
       <Global
         styles={css`
           ${reset}
+          body,
+          html {
+            width: 100%;
+            position: relative;
+            overflow-x: hidden;
+            font-family: "Nunito", sans-serif;
+            background-color: ${theme.colors.background};
+          }
         `}
       />
   );
--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -1,4 +1,18 @@
-const colors = {};
+const colors = {
+  background: "#FFD2D9",
+  backgroundBlur: "rgba(255, 210, 217, 0.8)",
+  primary: "#0129BD",
+  secondary: "#8875f8",
+  caution: "#FF0D23",
+  buttonDisabled: "#B1B6CA",
+  buttonSelected: "#DEE7FF",
+  text: "#1A1A1A",
+  subtext: "rgba(26, 26, 26, 0.7)",
+  placeholder: "rgba(26, 26, 26, 0.3)",
+  textboxBackground: "#F5F5F5",
+  white: "#FFFFFF",
+  spreadWarning: "#ff6600",
+};
 
 export type Colors = typeof colors;
 export default colors;
