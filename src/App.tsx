7aa1a5659a30a7b96ca4901a89b26c0f8e3dfcc5
maro <maro@delightlabs.io>
2022-12-01 17:50:59 +0900

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import { useCallback } from "react";
 import GlobalStyles from "styles/GlobalStyles";
 
 function App() {
-  const renderRoute = useCallback(({ children, ...props }: RouteObject) => {
-    return (
-      <Routes key={`${props.path} ${props.index}`} {...props}>
-        {children?.map(renderRoute)}
-      </Routes>
-    );
-  }, []);
+  const renderRoute = useCallback(
+    ({ children, element, index, ...props }: RouteObject) => {
+      return (
+        <Route
+          {...(index
+            ? { index: true }
+            : { children: children?.map(renderRoute) })}
+          key={`${props.path}`}
+          element={element}
+          {...props}
+        />
+      );
+    },
+    [],
+  );
 
   return (
     <>
