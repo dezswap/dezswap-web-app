14dd02b6f4bcaf24ef34b1c21ef7b6e98656fb7d
maro <maro@delightlabs.io>
2022-12-01 17:53:10 +0900

    feat: breakpoints and fix style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import routes, { RouteObject } from "routes";
 import { useCallback } from "react";
 import GlobalStyles from "styles/GlobalStyles";
+import { setConfiguration as setGridConfiguration } from "react-grid-system";
+
+setGridConfiguration({
+  // sm, md, lg, xl, xxl, xxxl
+  breakpoints: [375, 768, 992, 1280, 1600, 1920],
+  containerWidths: [360, 740, 960, 1140, 1540, 1810],
+});
 
 function App() {
   const renderRoute = useCallback(({ children, ...props }: RouteObject) => {
