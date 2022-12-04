898e9007e62cea15df7b85abfba98b880036738c
maro <maro@delightlabs.io>
2022-12-05 01:36:45 +0900

    feat: create message

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,76 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import iconCaution from "assets/icons/icon-caution.svg";
+
+type MessageProps = React.PropsWithChildren<{
+  variant?: "error"; // TODO: "default" | "success" | "error" | "warning";
+}>;
+
+const Wrapper = styled.div<Pick<MessageProps, "variant">>`
+  width: 100%;
+  height: auto;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+  padding: 12px 16px;
+  border-radius: 12px;
+  opacity: 0.6;
+  font-size: 14px;
+  font-weight: 700;
+
+  ${({ variant, theme }) => {
+    switch (variant) {
+      case "error":
+        return css`
+          background-color: ${theme.colors.danger};
+          color: ${theme.colors.white};
+        `;
+      default:
+        return css`
+          /* TODO */
+        `;
+    }
+  }}
+`;
+
+const Content = styled.div`
+  flex: 1;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Icon = styled.div<{ src?: string }>`
+  position: relative;
+  width: 24px;
+  height: 24px;
+
+  background-image: url(${({ src }) => src});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+`;
+
+const getIconSrc = (variant: MessageProps["variant"]) => {
+  switch (variant) {
+    case "error":
+      return iconCaution;
+    default:
+      return undefined;
+  }
+};
+
+function Message({ variant, children }: MessageProps) {
+  const iconSrc = getIconSrc(variant);
+  return (
+    <Wrapper variant={variant}>
+      {iconSrc && <Icon src={iconSrc} />}
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+}
+
+export default Message;
