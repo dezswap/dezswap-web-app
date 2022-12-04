79af908ae98708cb93075a50cfd567820eaa9c42
maro <maro@delightlabs.io>
2022-12-05 01:38:28 +0900

    feat: create icon button

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,74 @@
+import styled from "@emotion/styled";
+
+interface IconButtonProps {
+  size?: number;
+  icons?: {
+    default?: string;
+    hover?: string;
+    active?: string;
+  };
+}
+
+const IconButton = styled.button<IconButtonProps>`
+  position: relative;
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+  background-image: ${({ icons }) => `url(${icons?.default})`};
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+
+  &:hover,
+  &:active {
+    background-image: none;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: ${({ icons }) =>
+      `url(${icons?.hover || icons?.default})`};
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    opacity: 0;
+  }
+
+  &:hover::before {
+    opacity: 1;
+  }
+  &:active::before {
+    opacity: 0;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: ${({ icons }) =>
+      `url(${icons?.active ?? icons?.hover ?? icons?.default})`};
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    opacity: 0;
+  }
+
+  &:active::after {
+    opacity: 1;
+  }
+`;
+
+IconButton.defaultProps = { type: "button" };
+
+export default IconButton;
