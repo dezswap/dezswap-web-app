735dffc44459869ccab4f62da5dec0dcd0124ad0
maro <maro@delightlabs.io>
2022-12-07 00:48:37 +0900

    feat: create navbar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,48 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { NavLink, NavLinkProps } from "react-router-dom";
+
+interface NavBarProps {
+  items: NavLinkProps[];
+  flex?: boolean;
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: auto;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavItem = styled(NavLink)<{ flex?: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  opacity: 0.5;
+  text-align: center;
+  padding: 7px 0;
+  ${({ flex }) =>
+    flex &&
+    css`
+      flex: 1;
+    `}
+
+  &.active {
+    opacity: 1;
+  }
+`;
+
+function NavBar({ items, flex = true }: NavBarProps) {
+  return (
+    <Wrapper>
+      {items.map((navLink) => (
+        <NavItem {...navLink} flex={flex} />
+      ))}
+    </Wrapper>
+  );
+}
+
+export default NavBar;
