035fab0ac121bc1fa3f34010517d43916e8bebbe
maro <maro@delightlabs.io>
2022-12-08 09:04:52 +0900

    feat: scrollbar

--- src/styles/GlobalStyles/index.tsx
+++ src/styles/GlobalStyles/index.tsx
@@ -1,8 +1,8 @@
-import { css, Global } from "@emotion/react";
+import { css, Global, useTheme } from "@emotion/react";
 import reset from "emotion-reset";
-import theme from "styles/theme";
 
 function GlobalStyles() {
+  const theme = useTheme();
   return (
     <Global
       styles={css`
@@ -12,13 +12,31 @@ function GlobalStyles() {
           width: 100%;
           height: 100%;
           position: relative;
-          overflow-x: hidden;
           font-family: "Nunito", sans-serif;
           background-color: ${theme.colors.background};
         }
 
+        #root {
+          & > div {
+            overflow-x: hidden;
+          }
+        }
+
         * {
           box-sizing: border-box;
+
+          scrollbar-width: thin;
+          scrollbar-color: ${theme.colors.secondary} transparent;
+          &::-webkit-scrollbar {
+            width: 8px;
+          }
+          &::-webkit-scrollbar-thumb {
+            background-color: ${theme.colors.secondary};
+            border-radius: 30px;
+          }
+          &::-webkit-scrollbar-track {
+            background-color: transparent;
+          }
         }
       `}
     />
