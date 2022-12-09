22da56bca9f5e50db3d743c8a3ebbf48bbba042f
마로 <maro@delightlabs.io>
2022-12-10 07:38:36 +0900

    feat: support responsive design (#10)
    * feat: support responsive design
    
    * feat: config grid system

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import routes, { RouteObject } from "routes";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import GlobalStyles from "styles/GlobalStyles";
-import { setConfiguration as setGridConfiguration } from "react-grid-system";
+import {
+  setConfiguration as setGridConfiguration,
+  useScreenClass,
+} from "react-grid-system";
+import { gridConfiguration, SCREEN_CLASSES } from "constants/layout";
 
-setGridConfiguration({
-  // sm, md, lg, xl, xxl, xxxl
-  breakpoints: [375, 768, 992, 1280, 1600, 1920],
-  containerWidths: [360, 740, 960, 1140, 1540, 1810],
-});
+setGridConfiguration(gridConfiguration);
 
 function App() {
+  const screenClass = useScreenClass();
+  useEffect(() => {
+    SCREEN_CLASSES.forEach((item) => {
+      document.body.classList.toggle(item, screenClass === item);
+    });
+  }, [screenClass]);
+
   const renderRoute = useCallback(
     ({ children, element, index, ...props }: RouteObject) => {
       return (
