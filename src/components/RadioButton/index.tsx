5357439c99e030e7315d269ede036f2e8b882bce
마로 <maro@delightlabs.io>
2022-12-12 01:30:46 +0900

    feat: create radio button (#22)

---
+++ src/components/RadioButton/index.tsx
@@ -0,0 +1,75 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { forwardRef } from "react";
+
+interface StyledRadioButtonProps {
+  block?: boolean;
+}
+
+type RadioButtonProps = React.PropsWithChildren<
+  React.InputHTMLAttributes<HTMLInputElement> & StyledRadioButtonProps
+>;
+
+const Wrapper = styled.label<StyledRadioButtonProps>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  width: auto;
+  height: auto;
+  user-select: none;
+
+  ${({ block }) =>
+    block &&
+    css`
+      width: 100%;
+      display: flex;
+    `}
+
+  & > div {
+    width: 100%;
+    height: auto;
+    position: relative;
+    display: block;
+
+    font-size: 16px;
+    font-weight: 700;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: ${({ theme }) => theme.colors.text.primary};
+
+    padding: 8px 16px;
+    border-radius: 8px;
+    border: 1px solid ${({ theme }) => theme.colors.disabled};
+  }
+
+  & > input {
+    width: 0%;
+    height: 0%;
+    position: absolute;
+    left: -1px;
+    top: -1px;
+    opacity: 0;
+
+    &:checked + div {
+      background-color: ${({ theme }) => theme.colors.selected};
+      border-color: ${({ theme }) => theme.colors.primary};
+    }
+  }
+`;
+
+const RadioButton = forwardRef<HTMLInputElement, RadioButtonProps>(
+  ({ children, block = false, ...inputProps }, ref) => {
+    return (
+      <Wrapper block={block}>
+        <input type="radio" {...inputProps} ref={ref} />
+        <div>{children}</div>
+      </Wrapper>
+    );
+  },
+);
+
+export default RadioButton;
