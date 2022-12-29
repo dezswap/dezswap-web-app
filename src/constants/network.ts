db6710c27dbaf2a60dab5104539aa1bef187fe66
Joowon Yun <iam.yunjoowon@gmail.com>
2022-12-29 15:35:10 +0900

    feat: add to axlUSDC info

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -29,6 +29,15 @@ export const nativeTokens: Asset[] = [
     iconSrc: "https://assets.xpla.io/icon/svg/XPLA.svg",
     balance: "0",
   },
+  {
+    address: "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5",
+    decimals: 6,
+    name: "Axelar USD Coin",
+    symbol: "axlUSDC",
+    total_supply: "",
+    iconSrc: "https://assets.xpla.io/icon/svg/ibc/axlUSDC.svg",
+    balance: "0",
+  },
 ];
 
 export default networks;
