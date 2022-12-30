377486f6ff8ce8958ef4907acdf08edc252af79f
maro <maro@delightlabs.io>
2022-12-30 11:39:04 +0900

    fix: set tippy duration to `0`

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -2,7 +2,13 @@ import Tippy, { TippyProps } from "@tippyjs/react";
 
 function Tooltip({ children, ...props }: TippyProps) {
   return (
-    <Tippy maxWidth="250px" arrow={false} {...props} theme="light-border">
+    <Tippy
+      maxWidth="250px"
+      arrow={false}
+      {...props}
+      theme="light-border"
+      duration={0}
+    >
       {children}
     </Tippy>
   );
