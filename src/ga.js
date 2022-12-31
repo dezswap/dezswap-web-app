0bcd6d67b84c1feed69c46a5b0512324471ebfb3
maro <maro@delightlabs.io>
2022-12-31 11:09:36 +0900

    fix: ga.js

--- src/ga.js
+++ src/ga.js
@@ -1,12 +1,18 @@
+/* eslint-disable prefer-rest-params */
+/* eslint-disable no-undef */
 import packageJson from "../package.json";
 
 if (window.location.href.startsWith(packageJson.homepage)) {
   const script = document.createElement("script");
   script.src = "https://www.googletagmanager.com/gtag/js?id=G-Q2DG7CBFRS";
   script.async = true;
-  document.body.appendChild(script);
-
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push("js", new Date());
-  window.dataLayer.push("config", "G-Q2DG7CBFRS");
+  document.head.appendChild(script);
 }
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+window.gtag = gtag;
+gtag("js", new Date());
+gtag("config", "G-Q2DG7CBFRS");
