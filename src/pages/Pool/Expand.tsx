c39af616af7f3c2f86ac0ae64c41e61662b09686
maro <maro@delightlabs.io>
2023-01-04 00:59:37 +0900

    feat: pool

---
+++ src/pages/Pool/Expand.tsx
@@ -0,0 +1,77 @@
+import Box from "components/Box";
+import iconExpand from "assets/icons/icon-expand.svg";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Hr from "components/Hr";
+import { useState } from "react";
+
+type ExpandProps = React.PropsWithChildren<{
+  header?: React.ReactNode;
+  extra?: React.ReactNode | React.ReactNode[];
+  isOpen?: boolean;
+}>;
+
+const Wrapper = styled(Box)`
+  padding: 0;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.selected};
+  }
+`;
+
+const Header = styled.div`
+  position: relative;
+`;
+
+const Extra = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 20px;
+`;
+
+const Content = styled.div`
+  padding: 20px;
+  padding-top: 0;
+`;
+
+function Expand({ header, extra, isOpen: defaultOpen, children }: ExpandProps) {
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+  return (
+    <Wrapper onClick={() => setIsOpen((current) => !current)}>
+      <Header>
+        {header}
+        <Extra>
+          {extra}
+          <div
+            css={css`
+              width: 18px;
+              height: 18px;
+              background-image: url(${iconExpand});
+              background-size: contain;
+              background-repeat: no-repeat;
+              background-position: 50% 50%;
+              transform: rotate(${isOpen ? 180 : 0}deg);
+              margin-left: 2px;
+            `}
+          />
+        </Extra>
+      </Header>
+      {isOpen && (
+        <Content>
+          <Hr
+            css={css`
+              margin-bottom: 20px;
+            `}
+          />
+          {children}
+        </Content>
+      )}
+    </Wrapper>
+  );
+}
+
+export default Expand;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { Navigate, RouteProps } from "react-router-dom";
 import PlaygroundPage from "pages/Playground";
 import SwapPage from "pages/Trade/Swap";
-// import ProvidePage from "pages/Pool/Provide";
-// import WithdrawPage from "pages/Pool/Withdraw";
-// import PoolPage from "pages/Pool";
+import ProvidePage from "pages/Pool/Provide";
+import WithdrawPage from "pages/Pool/Withdraw";
+import PoolPage from "pages/Pool";
 import TradePage from "pages/Trade";
 
 export interface RouteObject extends Omit<RouteProps, "children"> {
@@ -23,16 +23,15 @@ const routes: RouteObject[] = [
       { path: "*", element: <Navigate replace to="swap" /> },
     ],
   },
-  // {
-  //   path: "pool",
-  //   element: <PoolPage />,
-  //   children: [
-  //     { path: "add-liquidity", element: <ProvidePage /> },
-  //     { path: "withdraw", element: <WithdrawPage /> },
-  //     { index: true, element: <Navigate replace to="add-liquidity" /> },
-  //     { path: "*", element: <Navigate replace to="add-liquidity" /> },
-  //   ],
-  // },
+  {
+    path: "pool",
+    element: <PoolPage />,
+    children: [
+      { path: "add-liquidity", element: <ProvidePage /> },
+      { path: "withdraw", element: <WithdrawPage /> },
+      { path: "*", element: <Navigate replace to="add-liquidity" /> },
+    ],
+  },
   { index: true, element: <Navigate replace to="trade" /> },
   { path: "*", element: <Navigate replace to="trade" /> },
 ];
