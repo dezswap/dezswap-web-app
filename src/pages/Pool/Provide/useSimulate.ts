3495c8440c2fd76e464858df1e805adeea88f2d7
jhlee-young <young@delightlabs.io>
2023-01-06 18:46:41 +0900

    feat: provide/withdraw simulation

--- src/pages/Pool/Provide/useSimulate.ts
+++ src/pages/Pool/Provide/useSimulate.ts
@@ -1,5 +1,92 @@
-const useSimulate = () => {
-  /* TODO: implement */
+import useAssets from "hooks/useAssets";
+import { useEffect, useMemo, useState } from "react";
+import usePool from "hooks/usePool";
+import { Numeric } from "@xpla/xpla.js";
+
+export interface ProvideSimulationResult {
+  estimatedAmount: string;
+  poolAssets: { address: string; amount: string }[];
+  percentageOfShare: string;
+  share: string;
+}
+
+const useSimulate = (
+  pairAddress: string,
+  assetAddress: string,
+  assetAmount: string,
+) => {
+  const pool = usePool(pairAddress);
+  const { getAsset } = useAssets();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFailed, setIsFailed] = useState(false);
+  const [result, setResult] = useState<ProvideSimulationResult>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsFailed(false);
+    const fetchProvideInfos = async () => {
+      try {
+        if (pool) {
+          const poolAssets = pool.assets.map((asset) => ({
+            address:
+              "token" in asset.info
+                ? asset.info.token.contract_addr
+                : asset.info.native_token.denom,
+            amount: asset.amount,
+          }));
+
+          if (poolAssets.length > 1) {
+            const [poolAsset1, poolAsset2] = poolAssets.sort((a) =>
+              a?.address === assetAddress ? -1 : 1,
+            );
+
+            const depositAmount = Numeric.parse(assetAmount);
+            if (Numeric.parse(pool.total_share).gt(0) && depositAmount.gt(0)) {
+              const share = depositAmount
+                .mul(pool.total_share)
+                .div(poolAsset1.amount)
+                .ceil();
+              const estimated = share
+                .mul(poolAsset2.amount)
+                .div(pool.total_share)
+                .ceil();
+
+              setResult({
+                estimatedAmount: estimated.toString(),
+                poolAssets,
+                percentageOfShare: share
+                  .mul(100)
+                  .div(share.plus(pool.total_share))
+                  .toString(),
+                share: share.toString(),
+              });
+              return;
+            }
+          }
+        }
+        setIsFailed(true);
+        setResult(undefined);
+      } catch (error) {
+        console.error(error);
+        setIsFailed(true);
+        setResult(undefined);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    const timerId = setTimeout(() => {
+      fetchProvideInfos();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [assetAddress, getAsset, assetAmount]);
+
+  return useMemo(
+    () => ({ ...result, isLoading, isFailed }),
+    [isFailed, isLoading, result],
+  );
 };
 
 export default useSimulate;
--- src/pages/Pool/Withdraw/useSimulate.ts
+++ src/pages/Pool/Withdraw/useSimulate.ts
@@ -1,5 +1,94 @@
-const useSimulate = () => {
-  /* TODO: implement */
+import { Numeric } from "@xpla/xpla.js";
+import { useEffect, useMemo, useState } from "react";
+import usePool from "hooks/usePool";
+import { useNetwork } from "hooks/useNetwork";
+import useAssets from "hooks/useAssets";
+
+export interface WithdrawSimulationResult {
+  estimatedAmount1: string;
+  estimatedAmount2: string;
+  poolAssets: { address: string; amount: string }[];
+  percentageOfShare: string;
+}
+
+const useSimulate = (
+  pairAddress: string,
+  liquidityTokenAddress: string,
+  amount: string,
+) => {
+  const { name: networkName } = useNetwork();
+  const pool = usePool(pairAddress);
+  const { getAsset } = useAssets();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFailed, setIsFailed] = useState(false);
+  const [result, setResult] = useState<WithdrawSimulationResult>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsFailed(false);
+    let isAborted = false;
+
+    const fetchPool = async () => {
+      try {
+        if (isAborted) {
+          return;
+        }
+
+        if (pool) {
+          const poolAssets = pool.assets.map((asset) => ({
+            address:
+              "token" in asset.info
+                ? asset.info.token.contract_addr
+                : asset.info.native_token.denom,
+            amount: asset.amount,
+          }));
+
+          if (
+            poolAssets &&
+            poolAssets.length > 1 &&
+            Numeric.parse(pool.total_share).gt(0) &&
+            Numeric.parse(amount).gt(0)
+          ) {
+            const amountInNumeric = Numeric.parse(amount);
+            setResult({
+              estimatedAmount1: amountInNumeric
+                .mul(poolAssets[0].amount)
+                .div(pool.total_share)
+                .floor()
+                .toString(),
+              estimatedAmount2: amountInNumeric
+                .mul(poolAssets[1].amount)
+                .div(pool.total_share)
+                .floor()
+                .toString(),
+              poolAssets,
+              percentageOfShare: amountInNumeric.mul(100).toString(),
+            });
+            return;
+          }
+        }
+        setIsFailed(true);
+        setResult(undefined);
+      } catch (error) {
+        console.log(error);
+        setIsFailed(true);
+        setResult(undefined);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPool();
+
+    return () => {
+      isAborted = true;
+    };
+  }, [pairAddress, liquidityTokenAddress, amount, getAsset, networkName, pool]);
+
+  return useMemo(
+    () => ({ ...result, isLoading, isFailed }),
+    [isFailed, isLoading, pool, result],
+  );
 };
 
 export default useSimulate;
