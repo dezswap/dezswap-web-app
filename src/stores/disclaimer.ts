a087dbe0582093569cc0d39ef8322e9fbfe6c6d8
maro <maro@delightlabs.io>
2023-05-16 18:19:30 +0900

    feat: update packages, polyfills

--- src/stores/disclaimer.ts
+++ src/stores/disclaimer.ts
@@ -1,12 +1,15 @@
 import { atomWithStorage } from "jotai/utils";
 import { atom } from "jotai";
 
-const lastSeen = atomWithStorage<string | undefined>("disclaimer", undefined);
+const lastSeen = atomWithStorage<string | undefined>(
+  "disclaimer",
+  localStorage.getItem("disclaimer") ?? undefined, // https://github.com/pmndrs/jotai/discussions/1737
+);
 
-const disclaimerLastSeenAtom = atom<Date | undefined, Date>(
+const disclaimerLastSeenAtom = atom<Date, [Date], void>(
   (get) => {
     const l = get(lastSeen);
-    return l ? new Date(l) : undefined;
+    return l ? new Date(l) : new Date(0);
   },
   (_get, set, update) => set(lastSeen, update.toString()),
 );
