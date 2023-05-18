6681bf0cd7a70e9b15e38888e40a3f6260755176
Bryan Rhee <psy2848048@gmail.com>
2023-05-18 11:57:17 +0900

    feat: apply GA4 tag manager

--- src/ga.js
+++ src/ga.js
@@ -1,18 +1,14 @@
-/* eslint-disable prefer-rest-params */
-/* eslint-disable no-undef */
-import packageJson from "../package.json";
-
-if (window.location.href.startsWith(packageJson.homepage)) {
-  const script = document.createElement("script");
-  script.src = "https://www.googletagmanager.com/gtag/js?id=G-Q2DG7CBFRS";
-  script.async = true;
-  document.head.appendChild(script);
-}
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-window.gtag = gtag;
-gtag("js", new Date());
-gtag("config", "G-Q2DG7CBFRS");
+(function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src ='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+	}
+  )(window, document, 'script', 'dataLayer', 'GTM-N8CGVDH');
