49826717366d4010277c94f8a22c70c50d35e846
maro <maro@delightlabs.io>
2024-03-21 20:09:09 +0000

    feat: show connect wallet modal before navigating to wallet page
    - upgraded `react-router-dom` to latest version

--- src/hooks/modals/useConnectWalletModal.tsx
+++ src/hooks/modals/useConnectWalletModal.tsx
@@ -1,10 +1,10 @@
 import ConnectWalletModal from "components/Modal/ConnectWalletModal";
 import { useMemo } from "react";
-import useGlobalElement from "../useGlobalElement";
-import useModal from "../useModal";
+import useGlobalElement from "hooks/useGlobalElement";
+import useHashModal from "hooks/useHashModal";
 
 const useConnectWalletModal = () => {
-  const modal = useModal();
+  const modal = useHashModal();
   const element = useMemo(
     () => (
       <ConnectWalletModal isOpen={modal.isOpen} onRequestClose={modal.close} />
