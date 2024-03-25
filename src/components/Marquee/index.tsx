0ea0d79fd2fddee4d76916ac0d6798fbbb14f433
maro <maro@delightlabs.io>
2024-03-25 04:24:19 +0000

    feat: make marquee scrollable

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 const MarqueeContainer = styled.div`
   display: flex;
@@ -7,15 +8,27 @@ const MarqueeContainer = styled.div`
   position: relative;
 `;
 
-const MarqueeContent = styled.div`
+const MarqueeContent = styled.div<{ isScrolling?: boolean }>`
   width: auto;
   position: relative;
   display: flex;
   white-space: nowrap;
   animation: marquee 20s linear infinite;
+  user-select: none;
+  touch-action: pan-y;
+
   &:hover {
     animation-play-state: paused;
   }
+
+  ${({ isScrolling }) =>
+    isScrolling &&
+    css`
+      animation-play-state: paused;
+      & > * {
+        pointer-events: none;
+      }
+    `}
 
   @keyframes marquee {
     0% {
@@ -40,13 +53,73 @@ interface MarqueeProps extends React.PropsWithChildren {
 
 function Marquee({ children, duration, direction = "left" }: MarqueeProps) {
   const animationDirection = direction === "left" ? "normal" : "reverse";
+  const isPointerDown = useRef(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handlePointerUp = (event: PointerEvent) => {
+      if (event.button === 0) {
+        isPointerDown.current = false;
+        setIsScrolling(false);
+      }
+    };
+    const handlePointerMove = (event: PointerEvent) => {
+      if (isPointerDown.current) {
+        const target = contentRef.current;
+        if (target) {
+          const targetWidth = target.getBoundingClientRect().width;
+          const animationDurationMs =
+            parseFloat(window.getComputedStyle(target).animationDuration) *
+            1000;
+          target.getAnimations().forEach((animation) => {
+            if (typeof animation.currentTime === "number") {
+              if (animation.currentTime < 0) {
+                // eslint-disable-next-line no-param-reassign
+                animation.currentTime =
+                  animationDurationMs + animation.currentTime;
+              }
+              // eslint-disable-next-line no-param-reassign
+              animation.currentTime -=
+                (event.movementX * animationDurationMs) / targetWidth;
+              setIsScrolling(true);
+            }
+          });
+        }
+      }
+    };
+    const handleDragStart = (event: DragEvent) => {
+      const target = event.target as HTMLElement;
+      if (contentRef.current?.contains(target)) {
+        event.preventDefault();
+      }
+    };
+    window.addEventListener("pointerup", handlePointerUp);
+    window.addEventListener("pointermove", handlePointerMove);
+    window.addEventListener("pointercancel", handlePointerUp);
+    window.addEventListener("dragstart", handleDragStart);
+    return () => {
+      window.removeEventListener("pointerup", handlePointerUp);
+      window.removeEventListener("pointermove", handlePointerMove);
+      window.removeEventListener("pointercancel", handlePointerUp);
+      window.removeEventListener("dragstart", handleDragStart);
+    };
+  }, []);
+
   return (
     <MarqueeContainer>
       <MarqueeContent
+        ref={contentRef}
+        isScrolling={isScrolling}
         style={{
           animationDuration:
             typeof duration === "number" ? `${duration}ms` : duration,
           animationDirection,
+        }}
+        onPointerDown={(event) => {
+          if (event.buttons === 1) {
+            isPointerDown.current = true;
+          }
         }}
       >
         {children}
