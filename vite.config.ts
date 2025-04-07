d49551ba1715e5b4611f14f7d22147c341a30309
woohee <woohee@delightlabs.io>
2025-04-07 18:28:26 +0900

    fix: add cosmos types for resolve type error and add alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -27,6 +28,11 @@ export default defineConfig(({ mode }) => {
         protocolImports: false,
       }),
     ],
+    resolve: {
+      alias: {
+        "@interchainjs": path.resolve(__dirname, "node_modules/@interchainjs"),
+      },
+    },
     server: {
       port: Number(env.port) || undefined,
       https: true,
