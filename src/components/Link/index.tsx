d97d955c19a7f650aacb41cbc614f8133cc2a194
JoowonYun <joowon@delightlabs.io>
2025-04-29 19:09:09 +0900

    fix: warning link

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,11 +1,14 @@
 import { Link as RRLink, LinkProps } from "react-router-dom";
 import { useFormatTo } from "hooks/useFormatTo";
+import React from "react";
 
-function Link({ to, ...props }: LinkProps) {
-  const { formatTo } = useFormatTo();
-  const formattedTo = formatTo({ to });
+const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  ({ to, ...props }, ref) => {
+    const { formatTo } = useFormatTo();
+    const formattedTo = formatTo({ to });
 
-  return <RRLink to={formattedTo} {...props} />;
-}
+    return <RRLink to={formattedTo} {...props} ref={ref} />;
+  },
+);
 
 export default Link;
