51866d6afe93a074a410f76222c8fbe4256d97dc
woohee Byun <woohee@delightlabs.io>
2025-06-05 19:10:43 +0900

    fix: change queryKey

--- src/hooks/usePlay3WhiteList.ts
+++ src/hooks/usePlay3WhiteList.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import useAPI from "./useAPI";
+import useNetwork from "./useNetwork";
 
 const usePlay3WhiteList = () => {
   const api = useAPI();
 
   const { data: whiteList = [] } = useQuery({
-    queryKey: ["whiteList"],
+    queryKey: ["play3WhiteList"],
     queryFn: async () => {
       const list = (await api.getPlay3List()) ?? [];
       return list.map((token) => token.cont_addr);
