ebba196c318a28b40fdef765c691341c3f1f6f06
woohee Byun <woohee@delightlabs.io>
2025-08-11 19:12:28 +0900

    refactor: get authInfo fuc

--- src/hooks/useAuthSequence.ts
+++ src/hooks/useAuthSequence.ts
@@ -1,9 +1,9 @@
+import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAPI from "./useAPI";
 import useNetwork from "./useNetwork";
 import useSigningClient from "./useSigningClient";
 import useConnectedWallet from "./useConnectedWallet";
-import { useMemo } from "react";
 
 function useAuthSequence() {
   const api = useAPI();
@@ -20,16 +20,9 @@ function useAuthSequence() {
       }
 
       try {
-        if (isXpla) {
-          const authInfo = await api.getAuthInfo();
-          const { sequence: authSequence } = authInfo || {};
-          return authSequence ?? 0n;
-        }
-        if (!client) {
-          throw new Error("Signing client is not available");
-        }
-        const clientSequence = await client.getSequence(walletAddress);
-        return clientSequence ?? 0n;
+        const authInfo = await api.getAuthInfo();
+        const { sequence: authSequence } = authInfo || {};
+        return authSequence ?? 0n;
       } catch (err) {
         console.error("Failed to get auth sequence:", err);
         throw err;
