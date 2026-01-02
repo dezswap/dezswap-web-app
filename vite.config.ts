5b277dcf242708dc0e66ca6d4aba21bc25d64881
Wonhee Lee <2wheeh@gmail.com>
2026-01-02 16:46:45 +0900

    feat: update versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import path from "path";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -28,11 +27,6 @@ export default defineConfig(({ mode }) => {
         protocolImports: false,
       }),
     ],
-    resolve: {
-      alias: {
-        "@interchainjs": path.resolve(__dirname, "node_modules/@interchainjs"),
-      },
-    },
     server: {
       port: Number(env.port) || undefined,
     },
